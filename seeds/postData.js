8673cb03cd7c56d6f75544f1ab1032790d56d271
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-06-13 15:00:31 -0500

    more seeds

--- seeds/postData.js
+++ seeds/postData.js
@@ -1,7 +1,54 @@
 const { Post } = require("../models");
 
 const postData = [
-    {}
+    {
+        title: "",
+        post_content: "",
+        user_id: 4
+    },
+
+    {
+        title: "",
+        post_content: "",
+        user_id: 2
+    },
+
+    {
+        title: "",
+        post_content: "",
+        user_id: 3
+    },
+
+    {
+        title: "",
+        post_content: "",
+        user_id: 1
+    },
+
+    {
+        title: "",
+        post_content: "",
+        user_id: 5
+    },
+
+    {
+        title: "",
+        post_content: "",
+        user_id: 3
+    },
+
+    {
+        title: "",
+        post_content: "",
+        user_id: 1
+    },
+
+    {
+        title: "",
+        post_content: "",
+        user_id: 4
+    },
+
   ]
   
 const postSeeds = () => Post.bulkCreate(postData);
