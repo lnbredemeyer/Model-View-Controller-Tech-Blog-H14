e2fa85f1633a62dcb091996cd590892019a8bf19
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-06-13 15:31:16 -0500

    seed info

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -1,7 +1,29 @@
 const { Comment } = require("../models");
 
 const commentData = [
-    {}
+    {
+        comment_text: "Seed Comment content 1",
+        user_id: 3,
+        post_id: 8
+    },
+
+    {
+        comment_text: "Seed Comment content 2",
+        user_id: 1,
+        post_id: 4
+    },
+
+    {
+        comment_text: "Seed Comment content 3",
+        user_id: 2,
+        post_id: 3
+    },
+
+    {
+        comment_text: "Seed Comment content 4",
+        user_id: 5,
+        post_id: 1
+    },
   ]
   
 const commentSeeds = () => Comment.bulkCreate(commentData);
--- seeds/postData.js
+++ seeds/postData.js
@@ -2,53 +2,52 @@ const { Post } = require("../models");
 
 const postData = [
     {
-        title: "",
-        post_content: "",
+        title: "Seed Post 1",
+        post_content: "Seed Post content 1, info following is just filler text. There is no truth here. Trust no one.",
         user_id: 4
     },
 
     {
-        title: "",
-        post_content: "",
+        title: "Seed Post 2",
+        post_content: "Seed Post content 2, info following is just filler text. There is no truth here. Trust no one.",
         user_id: 2
     },
 
     {
-        title: "",
-        post_content: "",
+        title: "Seed Post 3",
+        post_content: "Seed Post content 3, info following is just filler text. There is no truth here. Trust no one.",
         user_id: 3
     },
 
     {
-        title: "",
-        post_content: "",
+        title: "Seed Post 4",
+        post_content: "Seed Post content 4, info following is just filler text. There is no truth here. Trust no one.",
         user_id: 1
     },
 
     {
-        title: "",
-        post_content: "",
+        title: "Seed Post 5",
+        post_content: "Seed Post content 5, info following is just filler text. There is no truth here. Trust no one.",
         user_id: 5
     },
 
     {
-        title: "",
-        post_content: "",
+        title: "Seed Post 6",
+        post_content: "Seed Post content 6, info following is just filler text. There is no truth here. Trust no one.",
         user_id: 3
     },
 
     {
-        title: "",
-        post_content: "",
+        title: "Seed Post 7",
+        post_content: "Seed Post content 7, info following is just filler text. There is no truth here. Trust no one.",
         user_id: 1
     },
 
     {
-        title: "",
-        post_content: "",
+        title: "Seed Post 8",
+        post_content: "Seed Post content 8, info following is just filler text. There is no truth here. Trust no one.",
         user_id: 4
     },
-
   ]
   
 const postSeeds = () => Post.bulkCreate(postData);
