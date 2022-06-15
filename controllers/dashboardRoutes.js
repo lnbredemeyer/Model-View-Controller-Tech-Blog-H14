19f2239faa8b52ac7ff8d7a2c8247f045dc4eeac
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-06-15 11:09:46 -0500

    routes updates

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,24 +2,29 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+
 router.get('/', withAuth, (req, res) => {
   Post.findAll({
     where: {
       user_id: req.session.user_id,
     },
-    attributes: ['id', 'title', 'post_content', 'created_at'],
+    attributes: [
+      'id', 
+      'title', 
+      'post_content', 
+      'created_at'],
     include: [
       {
         model: Comment,
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ['name', 'email'],
+          attributes: ['name'],
         },
       },
       {
         model: User,
-        attributes: ['name', 'email'],
+        attributes: ['name'],
       },
     ],
   })
@@ -41,43 +46,42 @@ router.get('/edit/:id', withAuth, (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ['id', 'title', 'post_content', 'created_at'],
+    attributes: [
+      'id', 
+      'title', 
+      'post_content', 
+      'created_at'],
     include: [
       {
         model: User,
-        attributes: ['name', 'email'],
+        attributes: ['name'],
       },
       {
         model: Comment,
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ['name', 'email'],
+          attributes: ['name'],
         },
       },
     ],
   })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this ID' });
-        return;
-      }
-
+  .then(dbPostData => {
+    if (dbPostData) {
       const post = dbPostData.get({ plain: true });
-      console.log('sending ' + req.session.username);
+      
       res.render('edit-post', {
         post,
-        logged_in: true,
-        username: req.session.username,
+        loggedIn: true,
+        username: req.session.username
       });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-router.get('/new', (req, res) => {
-  res.render('new-post', { username: req.session.username });
+    } else {
+      res.status(404).end();
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
